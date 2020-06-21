14dcd9c6d00c58ce153261c08f51a4af733e7682
Nelly BORJON-PIRON <nelly.borjon@hotmail.fr>
2020-06-21 19:49:13 +0200

    Second part done - shorter way to write code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 // import { render } from '@testing-library/react';
+
+let business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+let businesses=[business, business, business, business, business, business]
 
 class App extends React.Component {
   render() {
@@ -11,10 +25,10 @@ class App extends React.Component {
       <div className="App">
         <h1>Ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
 }
 
-export default App
+export default App;
